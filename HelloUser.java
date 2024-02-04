d2e9e86760c3c080949634696b96db28c64aa028
Ganesh0311 <158815161+Ganesh0311@users.noreply.github.com>
2024-02-04 16:51:03 +0530

    Create HelloUser.java

---
+++ HelloUser.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HelloUser {
+   public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+
+       System.out.print("Enter your username: ");
+       String username = input.nextLine();
+
+       System.out.println("Hello " + username + "!");
+       input.close()
+   }
+}
