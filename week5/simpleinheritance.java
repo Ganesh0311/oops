07121b638585784fda3daf85a99de136da0c49db
Ganesh0311 <158815161+Ganesh0311@users.noreply.github.com>
2024-03-07 10:13:16 +0530

    Create simpleinheritance.java

---
+++ week5/simpleinheritance.java
@@ -0,0 +1,19 @@
+class college {
+    String collegeName = "MVGR";
+    int collegeCode = 33;
+}
+
+class student extends college {
+    String name = "ganeshi";
+    int roll = 520;
+}
+
+class SimpleInherit {
+    public static void main(String[] args) {
+        student student1 = new student();
+        System.out.println("Student name: "+student1.name);
+        System.out.println("Student roll num : "+student1.roll);
+        System.out.println("Student college name : "+student1.collegeName);
+    }
+}
+
