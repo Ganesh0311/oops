d80333db354a073ebd7001054c925a7cef7270e1
Ganesh0311 <158815161+Ganesh0311@users.noreply.github.com>
2024-03-21 07:25:06 +0530

    Create Overloading.java

---
+++ Week7/Overloading.java
@@ -0,0 +1,18 @@
+//import java.util.Scanner;
+class over{
+        public static void display(int a){
+            System.out.println("output of integer data "+a);
+        }
+        public static void display(String a){
+            System.out.println("output of string data "+a);
+        }
+        public static void display(double a){
+            System.out.println("output of double data "+a);
+        }
+        public static void main(String[] args){
+            display(1);
+            display("Hii Ayhkela");
+            display(6.0);
+
+        }
+}
