4eaf1df8f22df8a09be21fc4f1d6e1e1b5ca56e4
Ganesh0311 <158815161+Ganesh0311@users.noreply.github.com>
2024-04-18 08:52:42 +0530

    Create Boxandunboxing.java

---
+++ Week8/Boxandunboxing.java
@@ -0,0 +1,12 @@
+public class BoxandUnbox {
+    public static void main(String[] args)
+    {
+        //Boxing
+        Integer x = new Integer(60);
+        Integer y = new Integer(20);
+        //UnBoxing
+        int z=y;
+        System.out.println("Boxed Value : "+x);
+        System.out.println("UnBoxed Value : "+z);
+    }
+  }
